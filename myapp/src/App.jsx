5a95b0e6587e7a9c6bdbe518e644109f8018c435
PuneethKumar M S <puneethkumarms912@gmail.com>
2025-08-02 16:56:06 +0530

    removed node modules

---
+++ myapp/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FigureList from './components/FigureList';
+
+function App () {
+
+  return(
+
+    <div>
+      <FigureList/>
+    </div>
+  );
+}
+export default App;
